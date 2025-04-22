2357911e6260c10db174d5a9f8480fe334eb9828
daniel amekpoagbe <amekpoagbedaniel@gmail.com>
2025-04-22 07:51:53 +0000

    updating codes

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,7 @@ import Tabs from "../Components/Tabs";
 
 const Homepage = () => {
   return (
-    <div className="w-full min-h-screen bg-gray-200 text-gray-900 font-sans">
+    <div className="w-full min-h-screen bg-gray-200 text-gray-900 ">
       <HeroSection />
       <div className="p-1">
         <h2 className="text-3xl font-bold text-center mb-8">Browse Products</h2>
